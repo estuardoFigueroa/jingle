6db493324768a3b4cdbd6255c8d35dfd4a28476b
Estuardo Figueroa <estuardo.fig@gmail.com>
2014-11-20 14:37:46 -0600

    added a sales column on Jingle

--- jingleProject/app/admin/jingle.rb
+++ jingleProject/app/admin/jingle.rb
@@ -1,13 +1,13 @@
 ActiveAdmin.register Jingle do
-permit_params :genre_id, :name, :description, :price, :hasLyrics, :isBought, :image
+permit_params :genre_id, :name, :description, :price, :hasLyrics, :isBought, :image, :sale
 
   index do
     column :name
     column :genre
+    column :description
     column :price do |jingle|
       number_to_currency jingle.price
     end
-    column :description
     actions
   end
 end
