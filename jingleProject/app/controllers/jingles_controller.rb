c62fccf5f963e94562db60b77e49542aee92923a
Estuardo Figueroa <estuardo.fig@gmail.com>
2014-11-22 11:17:02 -0600

    Added All jingles by description, Jingles by sale, and Latest Jingles on the Jingles page

--- jingleProject/app/controllers/jingles_controller.rb
+++ jingleProject/app/controllers/jingles_controller.rb
@@ -1,13 +1,16 @@
 class JinglesController < ApplicationController
   def home
-    @jingles = Jingle.all
+    @jinAll = Jingle.all
   end
 
   def about
   end
 
   def jingles
+    @jingles = Jingle.all
     @newestJingles = Jingle.limit(3).reverse_order
     @onsaleJingles = Jingle.where("price < 250")
+    @allJingles    = Jingle.all
+    @jinGenre      = Genre.all
   end
 end
